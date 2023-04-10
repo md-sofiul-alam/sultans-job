f3a3bc337f3616712655a2acc4a17983084b5c9f
md-sofiul-alam <sowrobeakter@gmail.com>
2023-04-10 10:19:28 +0600

    tailwind css and react route implimented

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <h2 className='text-4xl font-bold text-blue-400'>This is banner</h2>
+        </div>
+    );
+};
+
+export default Banner;
